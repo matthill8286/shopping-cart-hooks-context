f3f410a45fedf1b5c63eefeb6227e11e978602c5
Matthew Hill <matthill8286@gmail.com>
2019-09-23 18:52:07 +0100

    stripping out code, removed mock server as using effects

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -13,16 +13,7 @@ class Cart extends PureComponent {
   static contextType = ShopContext;
 
   static propTypes = {
-    loadCart: PropTypes.func.isRequired,
-    updateCart: PropTypes.func.isRequired,
-    cartProducts: PropTypes.array.isRequired,
-    newProduct: PropTypes.object,
-    removeProduct: PropTypes.func,
-    productToRemove: PropTypes.object
-  };
-
-  state = {
-    isOpen: true
+    removeProductFromCart: PropTypes.func
   };
 
   get getCartTotal() {
@@ -43,125 +34,32 @@ class Cart extends PureComponent {
     }, 0)
   }
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (nextProps.newProduct !== this.props.newProduct) {
-      this.addProduct(nextProps.newProduct);
-    }
-
-    if (nextProps.productToRemove !== this.props.productToRemove) {
-      this.removeProduct(nextProps.productToRemove);
-    }
-  }
-
-  openCart = () => {
-    this.setState({ isOpen: true });
-  };
-
-  closeCart = () => {
-    this.setState({ isOpen: false });
-  };
-
-  addProduct = product => {
-    const { cartProducts, updateCart, rate } = this.props;
-    let productAlreadyInCart = false;
-
-    cartProducts.forEach(cp => {
-      if (cp.id === product.id) {
-        cp.quantity += product.quantity;
-        productAlreadyInCart = true;
-      }
-    });
-
-    if (!productAlreadyInCart) {
-      cartProducts.push(product);
-    }
-
-    updateCart(cartProducts, rate);
-    this.openCart();
-  };
-
-  removeProduct = product => {
-    const { cartProducts, updateCart, rate } = this.props;
-
-    const index = cartProducts.findIndex(p => p.id === product.id);
-    if (index >= 0) {
-      cartProducts.splice(index, 1);
-      updateCart(cartProducts, rate);
-    }
-  };
-
-  proceedToCheckout = () => {
-    const {
-      cartTotal: {
-        productQuantity
-      },
-      conversion: {
-        currencyId
-      }
-    } = this.props;
-
-    if (!productQuantity) {
-      alert('Add some product in the cart!');
-    } else {
-      alert(
-        `Checkout - Subtotal: ${currencyId} ${formatPrice(
-          this.getCartTotal
-        )}`
-      );
-    }
-  };
+  proceedToCheckout = () => null
 
   removeFromCart = (id) => {
     const { removeProductFromCart } = this.context;
     return removeProductFromCart(id)
   }
 
+  renderProducts = (cart) => cart.map(product => {
+    return (
+      <CartProduct
+        product={product}
+        removeProduct={this.removeFromCart.bind(this, product.id)}
+        key={product.id}
+      />
+    );
+  });
+
   render() {
     const { cart } = this.context;
-    const { isOpen } = this.state
-
-    const products = cart.map(product => {
-      return (
-        <CartProduct
-          product={product}
-          removeProduct={this.removeFromCart.bind(this, product.id)}
-          key={product.id}
-        />
-      );
-    });
-
-    let classes = ['float-cart'];
-
-    if (!!isOpen) {
-      classes.push('float-cart--open');
-    }
 
     return (
       <React.Fragment>
         <MainNavigation
           cartItemNumber={this.getCartItems}
         />
-        <div className={classes.join(' ')}>
-          {/* If cart open, show close (x) button */}
-          {isOpen && (
-            <div
-              onClick={() => this.closeCart()}
-              className="float-cart__close-btn"
-            >
-              X
-          </div>
-          )}
-
-          {/* If cart is closed, show bag with quantity of product and open cart action */}
-          {!isOpen && (
-            <span
-              onClick={() => this.openCart()}
-              className="bag bag--float-cart-closed"
-            >
-              <span className="bag__quantity">{this.getCartItems}</span>
-            </span>
-          )}
-
+        <div className="float-cart">
           <div className="float-cart__content">
             <div className="float-cart__header">
               <span className="bag">
@@ -171,8 +69,8 @@ class Cart extends PureComponent {
             </div>
 
             <div className="float-cart__shelf-container">
-              {products}
-              {!products.length && (
+              {this.renderProducts(cart)}
+              {!cart.length && (
                 <p className="shelf-empty">
                   Add some products in the cart <br />
                   :)
