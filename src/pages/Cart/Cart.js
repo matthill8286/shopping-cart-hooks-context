e3d2740af8c132d6b70cda30c2161fab15961aaa
matthill8286 <matthill8286@gmail.com>
2020-04-04 10:33:03 +0100

    Update Cart.js

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -8,10 +8,6 @@ import './styles.scss';
 class CartPage extends Component {
   static contextType = ShopContext;
 
-  componentDidMount() {
-    console.log(this.context);
-  }
-
   render() {
     return (
       <React.Fragment>
